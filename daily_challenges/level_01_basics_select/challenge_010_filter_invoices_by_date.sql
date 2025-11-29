82e8e3bec50deb13984abb99c0981b06d53b37b0
Daniel Jane <danij4ne@gmail.com>
2025-11-29 11:55:56 +0100

    exercise 10

---
+++ daily_challenges/level_01_basics_select/challenge_010_filter_invoices_by_date.sql
@@ -0,0 +1,18 @@
+
+-- CHALLENGE 010 - FILTER INVOICES BY DATE
+-- Difficulty: EASY
+-- Topic: WHERE + Date Filtering
+-- Database: Chinook
+
+-- DESCRIPTION:
+-- Retrieve invoices created after a specific date.
+-- This challenge focuses on filtering datetime values.
+
+-- REQUIREMENTS:
+--   1. Select the following columns from the Invoice table:
+--        - InvoiceId
+--        - CustomerId
+--        - InvoiceDate
+--        - Total
+--   2. Return only invoices with InvoiceDate greater than '2010-01-01'.
+--   3. Sort the results by InvoiceDate in ascending order.
