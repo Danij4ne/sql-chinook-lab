6b9abe7b2b07955677a24464ed03f4584c7b27d7
Daniel Jane <danij4ne@gmail.com>
2025-11-29 11:55:23 +0100

    exercise 07

---
+++ daily_challenges/level_01_basics_select/challenge_007_basic_where_conditions.sql
@@ -0,0 +1,19 @@
+
+-- CHALLENGE 007 - BASIC WHERE CONDITIONS
+-- Difficulty: EASY
+-- Topic: WHERE clause
+-- Database: Chinook
+
+-- DESCRIPTION:
+-- Practice filtering rows using basic comparison operators.
+-- This challenge focuses on applying simple WHERE conditions.
+
+-- REQUIREMENTS:
+--   1. Select the following columns from the Track table:
+--        - Name
+--        - Milliseconds
+--        - Bytes
+--   2. Return only tracks that meet ALL of the following conditions:
+--        a) Milliseconds is greater than 250000
+--        b) Bytes is greater than 5000000
+--   3. Sort the results by Milliseconds in descending order.
