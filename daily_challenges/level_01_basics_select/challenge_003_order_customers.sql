af3cefd73afa8798a84da330bd238bd437667099
Daniel Jane <danij4ne@gmail.com>
2025-11-29 11:54:41 +0100

    exercise 03

---
+++ daily_challenges/level_01_basics_select/challenge_003_order_customers.sql
@@ -0,0 +1,18 @@
+
+-- CHALLENGE 003 - ORDER CUSTOMERS
+-- Difficulty: EASY
+-- Topic: ORDER BY
+-- Database: Chinook
+
+-- DESCRIPTION:
+-- Retrieve a list of customers sorted in different ways.
+-- This challenge focuses on ordering query results using ORDER BY.
+
+-- REQUIREMENTS:
+--   1. Select the following columns from the Customer table:
+--        - FirstName
+--        - LastName
+--        - Country
+--   2. Return two different orderings in separate queries:
+--        a) Sort customers by Country (ascending).
+--        b) Sort customers by LastName (descending).
