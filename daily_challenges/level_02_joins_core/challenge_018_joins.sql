9b87cda618785a09bbf3e846264e7f0b6cd49299
Daniel Jane <danij4ne@gmail.com>
2025-12-12 08:44:39 +0100

    Completed challenge 018 add SQL query to join Track, Album, and Artist tables

--- daily_challenges/level_02_joins_core/challenge_018_joins.sql
+++ daily_challenges/level_02_joins_core/challenge_018_joins.sql
@@ -16,3 +16,15 @@
 --   2. Join Track → Album using AlbumId.
 --   3. Join Album → Artist using ArtistId.
 --   4. Sort the results alphabetically by ArtistName.
+
+SELECT 
+    t.Name AS TrackName,
+    al.Title AS AlbumTitle,
+    a.Name AS ArtistName
+FROM track t
+JOIN album al
+    ON t.AlbumId = al.AlbumId
+JOIN artist a
+    ON al.ArtistId = a.ArtistId
+ORDER BY a.Name ASC;
+
