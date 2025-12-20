3a15561206a16e1fbfcdb7ec7ee176432078d29e
Daniel Jane <danij4ne@gmail.com>
2025-12-20 14:50:55 +0100

    Completed challenge 028

--- daily_challenges/level_02_joins_core/challenge_028_joins.sql
+++ daily_challenges/level_02_joins_core/challenge_028_joins.sql
@@ -17,3 +17,16 @@
 --   2. Join Invoice → Customer using CustomerId.
 --   3. Return only invoices where Total is greater than 10.
 --   4. Sort the results by Country, then by Total in descending order.
+
+SELECT
+    I.InvoiceId,
+    I.InvoiceDate,
+    I.Total,
+    C.Country
+FROM Invoice AS I
+INNER JOIN Customer AS C
+    ON I.CustomerId = C.CustomerId
+WHERE I.Total > 10
+ORDER BY
+    C.Country,
+    I.Total DESC;
