3831a0c595136a44f12dce8213ed5dc85ca5cfc5
Daniel Jane <danij4ne@gmail.com>
2025-12-20 14:50:59 +0100

    Completed challenge 029

--- daily_challenges/level_02_joins_core/challenge_029_joins.sql
+++ daily_challenges/level_02_joins_core/challenge_029_joins.sql
@@ -17,3 +17,16 @@
 --   2. Join Track → Genre using GenreId.
 --   3. Join Track → MediaType using MediaTypeId.
 --   4. Sort the results alphabetically by TrackName.
+
+SELECT
+    T.TrackId,
+    T.Name AS TrackName,
+    G.Name AS GenreName,
+    M.Name AS MediaTypeName
+FROM Track AS T
+INNER JOIN Genre AS G
+    ON T.GenreId = G.GenreId
+INNER JOIN MediaType AS M
+    ON T.MediaTypeId = M.MediaTypeId
+ORDER BY
+    TrackName;
