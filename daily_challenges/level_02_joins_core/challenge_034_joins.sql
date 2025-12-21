66a23c8c2fc49d00ea3dc004774e30074b45d1b1
Daniel Jane <danij4ne@gmail.com>
2025-12-21 16:04:19 +0100

    Completed challenge 34

--- daily_challenges/level_02_joins_core/challenge_034_joins.sql
+++ daily_challenges/level_02_joins_core/challenge_034_joins.sql
@@ -17,3 +17,18 @@
 --   4. Join Track → InvoiceLine using TrackId.
 --   5. Group results by ArtistName.
 --   6. Sort the results by TotalSales in descending order.
+
+SELECT
+    ar.Name AS ArtistName,
+    SUM(il.UnitPrice * il.Quantity) AS TotalSales
+FROM Artist ar
+INNER JOIN Album al
+    ON ar.ArtistId = al.ArtistId
+INNER JOIN Track t
+    ON al.AlbumId = t.AlbumId
+INNER JOIN InvoiceLine il
+    ON t.TrackId = il.TrackId
+GROUP BY
+    ar.Name
+ORDER BY
+    TotalSales DESC;
