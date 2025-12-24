c6d34a601927f55ec38169158f63eaa04774b4cc
Daniel Jane <danij4ne@gmail.com>
2025-12-24 08:16:42 +0100

    Completed challenge 040

--- daily_challenges/level_03_group_by_aggregates/challenge_040_groupby.sql
+++ daily_challenges/level_03_group_by_aggregates/challenge_040_groupby.sql
@@ -20,3 +20,18 @@
 --        - Customer.FirstName
 --        - Customer.LastName
 --   4. Sort the results by InvoiceCount in descending order.
+
+SELECT
+    c.CustomerId,
+    c.FirstName,
+    c.LastName,
+    COUNT(i.InvoiceId) AS InvoiceCount
+FROM Customer c
+JOIN Invoice i
+    ON c.CustomerId = i.CustomerId
+GROUP BY
+    c.CustomerId,
+    c.FirstName,
+    c.LastName
+ORDER BY
+    InvoiceCount DESC;
