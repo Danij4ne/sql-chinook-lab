c74b73096a763addc91f5166d24fc92809064afd
Daniel Jane <danij4ne@gmail.com>
2025-12-28 14:00:20 +0100

    Completed challenge 051

--- daily_challenges/level_03_group_by_aggregates/challenge_051_groupby.sql
+++ daily_challenges/level_03_group_by_aggregates/challenge_051_groupby.sql
@@ -16,3 +16,16 @@
 --   3. Join Album → Track using AlbumId.
 --   4. Group results by ArtistName.
 --   5. Sort results by TrackCount in descending order.
+
+SELECT 
+    Artist.Name AS ArtistName,
+    COUNT(Track.TrackId) AS TrackCount
+FROM Artist
+JOIN Album 
+    ON Artist.ArtistId = Album.ArtistId
+JOIN Track 
+    ON Album.AlbumId = Track.AlbumId
+GROUP BY 
+    Artist.Name
+ORDER BY 
+    TrackCount DESC;
