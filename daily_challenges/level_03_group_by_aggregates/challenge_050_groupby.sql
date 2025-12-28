f61af490a5d447ec5350cb64a325628becbfcead
Daniel Jane <danij4ne@gmail.com>
2025-12-28 14:00:12 +0100

    Completed challenge 050

--- daily_challenges/level_03_group_by_aggregates/challenge_050_groupby.sql
+++ daily_challenges/level_03_group_by_aggregates/challenge_050_groupby.sql
@@ -20,3 +20,17 @@
 --        - Customer.FirstName
 --        - Customer.LastName
 --   4. Sort results by AvgTotal in descending order.
+
+SELECT 
+    Customer.CustomerId,
+    Customer.FirstName,
+    Customer.LastName,
+    AVG(Invoice.Total) AS AvgTotal
+FROM Customer
+JOIN Invoice 
+    ON Customer.CustomerId = Invoice.CustomerId
+GROUP BY 
+    Customer.CustomerId,
+    Customer.FirstName,
+    Customer.LastName
+ORDER BY AvgTotal DESC;
