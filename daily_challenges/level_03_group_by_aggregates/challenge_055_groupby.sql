c5f96a1eb1e6ccf906715bd85bad93d1fa6a6699
Daniel Jane <danij4ne@gmail.com>
2025-12-29 09:09:35 +0100

    Completed challenge 055

--- daily_challenges/level_03_group_by_aggregates/challenge_055_groupby.sql
+++ daily_challenges/level_03_group_by_aggregates/challenge_055_groupby.sql
@@ -16,3 +16,14 @@
 --   3. Join Track → InvoiceLine using TrackId.
 --   4. Group results by GenreName.
 --   5. Sort results by TotalSales in descending order.
+
+SELECT
+    Genre.Name AS GenreName,
+    SUM(InvoiceLine.UnitPrice * InvoiceLine.Quantity) AS TotalSales
+FROM Genre
+JOIN Track
+    ON Genre.GenreId = Track.GenreId
+JOIN InvoiceLine
+    ON Track.TrackId = InvoiceLine.TrackId
+GROUP BY Genre.Name
+ORDER BY TotalSales DESC;
