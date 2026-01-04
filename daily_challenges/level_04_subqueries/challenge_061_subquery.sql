18e5b9d1b41097748cabc8c7ac1db5edac1749b3
Daniel Jane <danij4ne@gmail.com>
2026-01-04 10:34:08 +0100

    Completed challenge 061

--- daily_challenges/level_04_subqueries/challenge_061_subquery.sql
+++ daily_challenges/level_04_subqueries/challenge_061_subquery.sql
@@ -19,3 +19,17 @@
 --   3. Return only invoices where:
 --        - Invoice.Total > average total for that same customer.
 --   4. Sort results by CustomerId, then InvoiceDate.
+ 
+
+SELECT
+    i.InvoiceId,
+    i.CustomerId,
+    i.InvoiceDate,
+    i.Total
+FROM Invoice i
+WHERE i.Total > (
+    SELECT AVG(i2.Total)
+    FROM Invoice i2
+    WHERE i2.CustomerId = i.CustomerId    
+)
+ORDER BY i.CustomerId, i.InvoiceDate;
