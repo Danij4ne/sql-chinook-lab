78325220c37f1d59ed260b3f5d8218befa6e67c8
Daniel Jane <danij4ne@gmail.com>
2026-01-05 09:06:34 +0100

    Completed challenge 062

--- daily_challenges/level_04_subqueries/challenge_062_subquery.sql
+++ daily_challenges/level_04_subqueries/challenge_062_subquery.sql
@@ -16,3 +16,25 @@
 --   3. Use a subquery to compute the average TrackCount across all genres.
 --   4. Return only genres whose TrackCount is greater than this average.
 --   5. Sort results by TrackCount in descending order.
+
+SELECT
+    g.Name AS GenreName,
+    COUNT(t.TrackId) AS TrackCount
+FROM Genre g
+JOIN Track t
+    ON t.GenreId = g.GenreId
+GROUP BY
+    g.GenreId,
+    g.Name
+HAVING
+    COUNT(t.TrackId) > (
+        SELECT AVG(genre_track_count)
+        FROM (
+            SELECT
+                COUNT(t2.TrackId) AS genre_track_count
+            FROM Track t2
+            GROUP BY t2.GenreId
+        ) x
+    )
+ORDER BY
+    TrackCount DESC;
