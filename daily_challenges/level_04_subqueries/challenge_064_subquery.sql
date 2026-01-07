21248bda945b81f0a2298c4252f1404dabf5ed9f
Daniel Jane <danij4ne@gmail.com>
2026-01-07 09:11:23 +0100

    Completed challenge 064

--- daily_challenges/level_04_subqueries/challenge_064_subquery.sql
+++ daily_challenges/level_04_subqueries/challenge_064_subquery.sql
@@ -19,3 +19,20 @@
 --   3. Return only tracks where:
 --        - Track.Milliseconds > average milliseconds for that same AlbumId.
 --   4. Sort results by AlbumId, then by Milliseconds in descending order.
+
+ 
+SELECT
+    t.TrackId,
+    t.Name,
+    t.AlbumId,
+    t.Milliseconds
+FROM Track t
+WHERE t.Milliseconds > (
+    SELECT AVG(tr.Milliseconds)
+    FROM Track tr
+    WHERE tr.AlbumId = t.AlbumId
+)
+ORDER BY
+    t.AlbumId,
+    t.Milliseconds DESC;
+    
