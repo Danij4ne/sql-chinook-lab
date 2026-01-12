9e84beddbe25cf2295907dc5d0705db849d7a293
Daniel Jane <danij4ne@gmail.com>
2026-01-12 15:08:12 +0100

    Completed challenge 068

--- daily_challenges/level_04_subqueries/challenge_068_subquery.sql
+++ daily_challenges/level_04_subqueries/challenge_068_subquery.sql
@@ -16,3 +16,16 @@
 --        - Select CustomerId, FirstName, LastName, TotalSpent.
 --   3. Order the results by TotalSpent in descending order.
 --   4. Return only the top 5 rows.
+
+SELECT TOP 5
+    t.CustomerId, t.FirstName, t.LastName, t.TotalSpent
+FROM (
+    SELECT
+        c.CustomerId, c.FirstName, c.LastName,
+        SUM(i.Total) AS TotalSpent
+    FROM Customer c
+    JOIN Invoice i ON i.CustomerId = c.CustomerId
+    GROUP BY c.CustomerId, c.FirstName, c.LastName
+) t
+ORDER BY t.TotalSpent DESC;
+
