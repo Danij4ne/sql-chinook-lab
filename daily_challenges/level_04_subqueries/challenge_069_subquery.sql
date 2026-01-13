9fd34cf086cca12cf01e501816f3319c79e0df16
Daniel Jane <danij4ne@gmail.com>
2026-01-13 12:32:59 +0100

    Completed challenge 069

--- daily_challenges/level_04_subqueries/challenge_069_subquery.sql
+++ daily_challenges/level_04_subqueries/challenge_069_subquery.sql
@@ -19,3 +19,20 @@
 --        - Select TrackId and Name from the Track table.
 --        - Return only tracks whose Album belongs to that artist.
 --   3. Sort results alphabetically by Track.Name.
+
+
+SELECT
+    t.TrackId,
+    t.Name
+FROM Track t
+JOIN Album al ON al.AlbumId = t.AlbumId
+WHERE al.ArtistId = (
+    SELECT TOP 1 ar.ArtistId
+    FROM Artist ar
+    JOIN Album al2      ON al2.ArtistId = ar.ArtistId
+    JOIN Track t2       ON t2.AlbumId = al2.AlbumId
+    JOIN InvoiceLine il ON il.TrackId = t2.TrackId
+    GROUP BY ar.ArtistId
+    ORDER BY SUM(il.UnitPrice * il.Quantity) DESC
+)
+ORDER BY t.Name;
