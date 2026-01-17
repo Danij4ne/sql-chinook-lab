7e959b2a808c5b0edfbf13e0509d3d735db38919
Daniel Jane <danij4ne@gmail.com>
2026-01-17 13:10:06 +0100

    Completed challenge 071

--- daily_challenges/level_05_window_functions/challenge_071_window.sql
+++ daily_challenges/level_05_window_functions/challenge_071_window.sql
@@ -11,3 +11,7 @@
 --   1. Select TrackId, Name, UnitPrice.
 --   2. Add a ROW_NUMBER() window function ordered by Name.
 --   3. Return the full result set.
+
+SELECT TrackId , Name , UnitPrice ,
+ROW_NUMBER() OVER(ORDER BY Name)
+FROM tracks 
