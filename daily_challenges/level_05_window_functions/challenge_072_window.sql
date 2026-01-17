27ddb6bba0bd763c516b6cb81c1bfab78179b4e1
Daniel Jane <danij4ne@gmail.com>
2026-01-17 15:39:41 +0100

    Completed challenge 072

--- daily_challenges/level_05_window_functions/challenge_072_window.sql
+++ daily_challenges/level_05_window_functions/challenge_072_window.sql
@@ -12,3 +12,10 @@
 --   2. Calculate SUM(Invoice.Total) AS TotalSpent.
 --   3. Use RANK() OVER (ORDER BY TotalSpent DESC).
 --   4. Return CustomerId, FirstName, LastName, TotalSpent, Rank.
+
+SELECT CustomerId , FirstName , LastName ,  SUM(I.Total) AS TotalSpent, 
+RANK() OVER(ORDER BY SUM(I.Total) DESC) AS Rank
+FROM Invoice i
+JOIN Customer c
+ON  c.CustomerId = i.CustomerId
+GROUP BY CustomerId , FirstName , LastName 
