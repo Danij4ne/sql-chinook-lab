4b2ea0d5b45da4040144de74adea0829a120980b
Daniel Jane <danij4ne@gmail.com>
2026-01-22 12:19:52 +0100

    Completed challenge 078

--- daily_challenges/level_05_window_functions/challenge_078_window.sql
+++ daily_challenges/level_05_window_functions/challenge_078_window.sql
@@ -11,3 +11,15 @@
 --   1. Select TrackId, Name, GenreId, Milliseconds.
 --   2. Use PERCENT_RANK() OVER (PARTITION BY GenreId ORDER BY Milliseconds).
 --   3. Order results by GenreId, then Milliseconds.
+
+SELECT 
+    TrackId,
+    Name,
+    GenreId,
+    Milliseconds,
+    PERCENT_RANK() OVER (
+        PARTITION BY GenreId 
+        ORDER BY Milliseconds
+    ) AS DurationPercentile
+FROM Track
+ORDER BY GenreId, Milliseconds;
