94bff176f0f493d59789edf3d80c37b4fd253ab8
Daniel Jane <danij4ne@gmail.com>
2026-01-26 12:44:14 +0100

    Completed challenge 088

--- daily_challenges/level_06_cte_advanced/challenge_088_cte.sql
+++ daily_challenges/level_06_cte_advanced/challenge_088_cte.sql
@@ -23,3 +23,24 @@
 --             - InvoiceCount
 --             - TotalSpent
 --   3. Order results by TotalSpent in descending order.
+
+WITH customer_invoices AS (
+    SELECT
+        CustomerId,
+        COUNT(InvoiceId) AS InvoiceCount,
+        SUM(Total)      AS TotalSpent
+    FROM Invoice
+    GROUP BY
+        CustomerId
+)
+SELECT
+    c.CustomerId,
+    c.FirstName,
+    c.LastName,
+    ci.InvoiceCount,
+    ci.TotalSpent
+FROM Customer c
+JOIN customer_invoices ci
+    ON ci.CustomerId = c.CustomerId
+ORDER BY
+    ci.TotalSpent DESC;
