1d136f4139f26b20b3e057d696e0a596fef99940
Daniel Jane <danij4ne@gmail.com>
2026-01-26 13:10:19 +0100

    Completed challenge 090

--- daily_challenges/level_06_cte_advanced/challenge_090_cte.sql
+++ daily_challenges/level_06_cte_advanced/challenge_090_cte.sql
@@ -19,3 +19,39 @@
 --        - ReportsTo
 --        - A level or depth column indicating the hierarchy level.
 --   4. Order results by the hierarchy level, then by LastName.
+ 
+
+WITH employee_hierarchy AS (
+
+    SELECT
+        e.EmployeeId,
+        e.FirstName,
+        e.LastName,
+        e.ReportsTo,
+        0 AS [Level]
+    FROM Employee e
+    WHERE e.ReportsTo IS NULL
+
+    UNION ALL
+
+    SELECT
+        e.EmployeeId,
+        e.FirstName,
+        e.LastName,
+        e.ReportsTo,
+        eh.[Level] + 1 AS [Level]
+    FROM Employee e
+    JOIN employee_hierarchy eh
+        ON e.ReportsTo = eh.EmployeeId
+)
+SELECT
+    EmployeeId,
+    FirstName,
+    LastName,
+    ReportsTo,
+    [Level]
+FROM employee_hierarchy
+ORDER BY
+    [Level],
+    LastName
+OPTION (MAXRECURSION 100);
