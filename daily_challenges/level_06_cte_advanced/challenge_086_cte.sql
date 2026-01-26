e0770eea69c3f13088cb48fba9287323dddd52a4
Daniel Jane <danij4ne@gmail.com>
2026-01-26 12:40:49 +0100

    Completed challenge 086

--- daily_challenges/level_06_cte_advanced/challenge_086_cte.sql
+++ daily_challenges/level_06_cte_advanced/challenge_086_cte.sql
@@ -19,3 +19,28 @@
 --        - TotalSpent
 --        - Rank
 --   4. Order results by TotalSpent in descending order.
+
+WITH customer_spending AS (
+    SELECT
+        c.CustomerId,
+        SUM(i.Total) AS TotalSpent
+    FROM Customer c
+    JOIN Invoice i
+        ON i.CustomerId = c.CustomerId
+    GROUP BY
+        c.CustomerId
+),
+ranked_customers AS (
+    SELECT
+        CustomerId,
+        TotalSpent,
+        RANK() OVER (ORDER BY TotalSpent DESC) AS [Rank]
+    FROM customer_spending
+)
+SELECT
+    CustomerId,
+    TotalSpent,
+    [Rank]
+FROM ranked_customers
+ORDER BY
+    TotalSpent DESC;
