4c493b1d1e567a13e75456cf4e910aebe61e7c0c
Daniel Jane <danij4ne@gmail.com>
2026-01-26 12:40:42 +0100

    Completed challenge 085

--- daily_challenges/level_06_cte_advanced/challenge_085_cte.sql
+++ daily_challenges/level_06_cte_advanced/challenge_085_cte.sql
@@ -17,3 +17,22 @@
 --        - Month
 --        - MonthlySales
 --   3. Order results by Year, then Month.
+
+WITH monthly_sales AS (
+    SELECT
+        YEAR(InvoiceDate)  AS [Year],
+        MONTH(InvoiceDate) AS [Month],
+        SUM(Total)         AS MonthlySales
+    FROM Invoice
+    GROUP BY
+        YEAR(InvoiceDate),
+        MONTH(InvoiceDate)
+)
+SELECT
+    [Year],
+    [Month],
+    MonthlySales
+FROM monthly_sales
+ORDER BY
+    [Year],
+    [Month];
